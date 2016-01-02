5d7efbc5595bcc397ce02e5e90cbeb2cad39ef0a
Nicolas Feller <nrkfeller@gmail.com>
2016-01-02 23:22:01 +0000

    List exercise on index tml

---
+++ spec/features/listing_exercises_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "Listing exercises" do
+    before do
+        @john = User.create(email: "john@email.com", password: "password1")
+        login_as(@john)
+        
+        @e1 = @john.exercises.create(duration_in_min: 20, workout: "bodybuilding", workout_date: "2015-08-09")
+        @e2 = @john.exercises.create(duration_in_min: 100, workout: "running", workout_date: "2015-08-19")
+    end
+    
+    scenario "Shows user's workouts for last 7 days" do
+        visit "/"
+        
+        click_link "My Lounge"
+        expect(page).to have_content(@e1.duration_in_min)
+        expect(page).to have_content(@e1.workout)
+        expect(page).to have_content(@e1.workout_date)
+        
+        expect(page).to have_content(@e2.duration_in_min)
+        expect(page).to have_content(@e2.workout)
+        expect(page).to have_content(@e2.workout_date)
+    end
+    
+    
+        
+    
+    
+end
